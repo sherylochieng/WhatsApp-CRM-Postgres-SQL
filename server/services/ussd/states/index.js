8792e00bbed799561f79b5706b36755f56c3bd8f
sherylOchieng <sadhiambo998@gmail.com>
2026-07-08 19:20:00 +0300

    feat:add index.js for states in states folder

---
+++ server/services/ussd/states/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  welcome: require("./welcome"),
+  my_tickets: require("./my_tickets"),
+  new_ticket_category: require("./new_ticket_category"),
+  new_ticket_message: require("./new_ticket_message"),
+  new_ticket_confirm: require("./new_ticket_confirm"),
+};
